57745312fd533ebefb1c080efb1b42d09c32d72e
EZmodeApps <75046274+EZmodeApps@users.noreply.github.com>
2022-04-10 04:31:06 +0300

    ex01 initial commit

---
+++ ex01/ClapTrap.h
@@ -0,0 +1,26 @@
+#ifndef CPP_MODULE_03_CLAPTRAP_H
+#define CPP_MODULE_03_CLAPTRAP_H
+
+#include <iostream>
+#include <string>
+
+class ClapTrap {
+
+public:
+	ClapTrap();
+	ClapTrap(std::string name);
+	~ClapTrap();
+
+	void attack(const std::string &target);
+	void takeDamage(unsigned int amount);
+	void beRepaired(unsigned int amount);
+
+private:
+	std::string name;
+	unsigned int hitPoints;
+	unsigned int energyPoints;
+	unsigned int damage;
+};
+
+
+#endif //CPP_MODULE_03_CLAPTRAP_H
